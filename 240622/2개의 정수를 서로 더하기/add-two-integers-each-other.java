87124f8bb8716a6df27bea1cca87699929a14170
dongkyukim1 <wlsntus55@gmail.com>
2024-06-22 19:35:36 +0900

    Title: 2개의 정수를 서로 더하기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240622/2개의 정수를 서로 더하기/add-two-integers-each-other.java
+++ 240622/2개의 정수를 서로 더하기/add-two-integers-each-other.java
@@ -2,19 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 변수 선언 및 입력
         Scanner sc = new Scanner(System.in);
-
-        // 두 정수를 입력받음
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        // b에 a를 더한 값을 b에 저장
-        b = b + a;
+        a += b;
+        b += a;
 
-        // a에 b를 더한 값을 a에 저장
-        a = b + a; 
-
-        // 결과 출력
         System.out.println(a + " " + b);
     }
 }
