d5091f0dbe7780f093b573a22e80bda3e22f5c4e
dongkyukim1 <wlsntus55@gmail.com>
2024-06-22 19:07:57 +0900

    Title: 두수의 곱과 몫, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240622/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
+++ 240622/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 두 정수를 입력받음
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println("300 * 5 = "+a*b);
-        System.out.println("300 / 5 = "+a/b);
+
+        // 곱셈 결과와 식 출력
+        int multiplicationResult = a * b;
+        System.out.println(a + " * " + b + " = " + multiplicationResult);
+
+        // 나눗셈 결과와 식 출력
+        int divisionResult = a / b;
+        System.out.println(a + " / " + b + " = " + divisionResult);
     }
 }
