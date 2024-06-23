44a30e22c6661aec013d9bc8d23e2884a2dc1b59
dongkyukim1 <wlsntus55@gmail.com>
2024-06-23 20:07:44 +0900

    Title: 점수 비교, Time: 245ms, Memory: 10MB, Status: Passed - CodeTree

--- 240623/점수 비교/score-comparison.java
+++ 240623/점수 비교/score-comparison.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
+       
        int math_a = sc.nextInt();
-       int math_b = sc.nextInt();
        int eng_a = sc.nextInt();
+       
+       int math_b = sc.nextInt();
        int eng_b = sc.nextInt();
 
 
