eecbe33d66ebbc80da3f9b2e64ea532b47dacb0e
dongkyukim1 <wlsntus55@gmail.com>
2024-06-23 21:16:33 +0900

    Title: 세 수의 중간값, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240623/세 수의 중간값/median-of-three-numbers.java
+++ 240623/세 수의 중간값/median-of-three-numbers.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int a = sc.nextInt();
-       int b = sc.nextInt();
-       int c = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-       if(b>a){
-        if(b<c){
+        if (b > a && b < c) {
             System.out.println(1);
-        }else{
+        } else {
             System.out.println(0);
         }
-       }
+
+        sc.close(); // Scanner 닫기
     }
 }
