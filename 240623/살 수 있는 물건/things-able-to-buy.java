ca1c71c46927ef088dac92ffdacb2abf54ad90cd
dongkyukim1 <wlsntus55@gmail.com>
2024-06-23 13:13:47 +0900

    Title: 살 수 있는 물건, Time: 232ms, Memory: 10MB, Status: Passed - CodeTree

--- 240623/살 수 있는 물건/things-able-to-buy.java
+++ 240623/살 수 있는 물건/things-able-to-buy.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int a = sc.nextInt();
-        if(a>=3000){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if (n >= 3000) {
             System.out.println("book");
-         }else if(a>=1000){
+        } else if (n >= 1000) {
             System.out.println("mask");
-        }else if (a==0){
+        } else {
             System.out.println("no");
         }
     }
