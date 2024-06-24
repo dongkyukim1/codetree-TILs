5e119ae42fea67d9494a258239fa636943fb605f
dongkyukim1 <wlsntus55@gmail.com>
2024-06-24 15:39:07 +0900

    Title: 차례로 출력, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240624/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       int a = 5;
+       int b = 17;
+       for(int i = a ; i<=b;i++){
+        System.out.printf(i+ " ");
+       }
+    }
+}
