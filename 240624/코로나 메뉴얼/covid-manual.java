772dc0370291319ec9f5443b110493b2139bcabc
dongkyukim1 <wlsntus55@gmail.com>
2024-06-24 14:17:46 +0900

    Title: 코로나 메뉴얼, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240624/코로나 메뉴얼/covid-manual.java
+++ 240624/코로나 메뉴얼/covid-manual.java
@@ -1,31 +1,40 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      
-      String b = sc.next();
-      int a = sc.nextInt();
-      String rs;
+        Scanner sc = new Scanner(System.in);
+        
+        int aCount = 0; // 'A'의 개수를 세기 위한 변수
+        
+        // 3명의 입력을 처리합니다.
+        for (int i = 0; i < 3; i++) {
+            String b = sc.next();
+            int a = sc.nextInt();
+            String rs = "";
 
-      if(b.equals("Y")){
-        if(a>=37){
-            rs = "A";
-        }else{
-            rs ="C";
+            if (b.equals("Y")) {
+                if (a >= 37) {
+                    rs = "A";
+                    aCount++;
+                } else {
+                    rs = "C";
+                }
+            } else if (b.equals("N")) {
+                if (a >= 37) {
+                    rs = "B";
+                } else {
+                    rs = "D";
+                }
+            }
         }
-      if(b.equals("N")){
-        if(a>=37){
-           rs = "B";     
-        }else{
-           rs ="D"; 
-        }  
-      }
-      if(rs.equals("A")){
-        System.out.println("E");
-      }else{
-        System.out.println("N");
-      } 
-   
+
+        // 위급상황 판단
+        if (aCount >= 2) {
+            System.out.println("E");
+        } else {
+            System.out.println("N");
+        }
+
+        sc.close();
     }
-  }
 }
