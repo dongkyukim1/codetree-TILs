505a7e115fb7bd583c9dd611a5348622a493b55d
dongkyukim1 <wlsntus55@gmail.com>
2024-06-24 20:02:43 +0900

    Title: 홀수만 출력, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240624/홀수만 출력/output-only-odd.java
+++ 240624/홀수만 출력/output-only-odd.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int a = sc.nextInt();
-       int b = sc.nextInt();
-    for(int i = 1; i<=b;i+=2){
-        System.out.print(i+" ");
-    }
-
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        
+        // a 이상 b 이하의 홀수 출력
+        for (int i = a; i <= b; i += 2) {
+            System.out.print(i + " ");
+        }
     }
 }
