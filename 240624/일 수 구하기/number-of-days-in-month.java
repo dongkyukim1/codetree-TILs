bf5e098492fd387994f3d1d8058bfb0a5af0505f
dongkyukim1 <wlsntus55@gmail.com>
2024-06-24 12:18:18 +0900

    Title: 일 수 구하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240624/일 수 구하기/number-of-days-in-month.java
+++ 240624/일 수 구하기/number-of-days-in-month.java
@@ -6,10 +6,11 @@ public class Main {
         
     if((a % 2 == 1 && a<9) || (a % 2 ==0 && a>7)){
 		System.out.println(31);
-        }else if((a % 2 ==0 && a<7) || (a % 2==1 && a>7)){
-        System.out.println(30);
         }else if(a==2){
             System.out.println(28);
+        }else if((a % 2 ==0 && a<7) || (a % 2==1 && a>7)){
+        System.out.println(30);
         }
+    
     }
 }
