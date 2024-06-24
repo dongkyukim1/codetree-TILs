2da37c068ddbb2c2e372315fa4bcf56201533a76
dongkyukim1 <wlsntus55@gmail.com>
2024-06-24 10:58:00 +0900

    Title: 윤년인가, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240624/윤년인가/is-leap-year.java
+++ 240624/윤년인가/is-leap-year.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a  = sc.nextInt();
-            
-            if(a % 4 == 0){
-             }if(a % 100 ==0){
-                System.out.println("false");
-            }else{
-                System.out.println("true");
-            }
+        int a = sc.nextInt();
+        
+        // 윤년 판단 로직
+        if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
         }
     }
+}
