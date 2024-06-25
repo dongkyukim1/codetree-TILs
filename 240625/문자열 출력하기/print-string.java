392ae836147072d4c1c2da11a1344098d1250f3d
dongkyukim1 <wlsntus55@gmail.com>
2024-06-25 21:14:29 +0900

    Title: 문자열 출력하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240625/문자열 출력하기/print-string.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        for(int a = sc.nextInt(); a<=11;a++ ){
+            System.out.println("LeebrosCode");
+        }
+    }
+}
