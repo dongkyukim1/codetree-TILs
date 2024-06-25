97dd244d03a09d03be14f2e50e66354f56372189
dongkyukim1 <wlsntus55@gmail.com>
2024-06-25 21:04:10 +0900

    Title: 짝수만 출력 2, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240625/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        while(a>=b){
+            System.out.print(a+" ");
+            a-=2;
+        }
+    }
+}
