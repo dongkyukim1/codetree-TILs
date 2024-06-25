efa5686a323bec308a8933aa888cfc81f2f34525
dongkyukim1 <wlsntus55@gmail.com>
2024-06-25 21:36:39 +0900

    Title: n번 더하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240625/n번 더하기/add-n-times.java
+++ 240625/n번 더하기/add-n-times.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-       int a = 5;
-       int n = 6;
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int n = sc.nextInt();
 
-       for(int i = 0 ; i<6;i++){
+       for(int i = 0 ; i<n;i++){
         System.out.println(a+n+(i*n));
        }
 
