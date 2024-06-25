16c2ee86306db2be51f8de89b1ee6b8851608d4b
dongkyukim1 <wlsntus55@gmail.com>
2024-06-25 21:35:27 +0900

    Title: n번 더하기, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240625/n번 더하기/add-n-times.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       int a = 5;
+       int n = 6;
+
+       for(int i = 0 ; i<6;i++){
+        System.out.println(a+n+(i*n));
+       }
+
+    }
+}
