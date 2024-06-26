30b225d25281aaf3ad4589a9cb6ff47c9d363f40
dongkyukim1 <wlsntus55@gmail.com>
2024-06-26 23:31:21 +0900

    Title: 369 게임, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240626/369 게임/369-game.java
+++ 240626/369 게임/369-game.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        for(int i = 1 ; i<=a ; i++){
-            if(i%3!=0){
-                System.out.print(i+" ");
-            }else{
-                System.out.print(0+" ");    
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || contains369(i)) {
+                System.out.print(0 + " ");
+            } else {
+                System.out.print(i + " ");
             }
         }
+    }
+
+    // 숫자에 3, 6, 9가 포함되어 있는지 확인하는 함수
+    public static boolean contains369(int num) {
+        String strNum = Integer.toString(num);
+        return strNum.contains("3") || strNum.contains("6") || strNum.contains("9");
     }
 }
