287772d69e7211fcc8c3779c34e2da7b780e8d46
dongkyukim1 <wlsntus55@gmail.com>
2024-06-27 00:03:18 +0900

    Title: 청소당번 정하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240627/청소당번 정하기/cleaning-numbering.java
+++ 240627/청소당번 정하기/cleaning-numbering.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,17 +9,15 @@ public class Main {
         int toliet_clean = 0;
         int n = sc.nextInt();
 
-        for(int i = 1 ; i<n ;i++){
-            if(i%2==0){
+        for (int i = 1; i < n; i++) {
+            if (i % 12 == 0) {
+                toliet_clean++;
+            } else if (i % 3 == 0) {
+                road_clean++;
+            } else if (i % 2 == 0) {
                 class_clean++;
             }
-            if(i%3==0){
-                road_clean++;
-            }
-            if(i%12==0){
-                toliet_clean++;
-            }
         }
-            System.out.print((class_clean-1)+" "+road_clean+" "+toliet_clean);
+        System.out.print(class_clean + " " + road_clean + " " + toliet_clean);
     }
 }
