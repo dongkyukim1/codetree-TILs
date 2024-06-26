28ed56ffb9488ba037a90da2e6bf90e6651e7137
dongkyukim1 <wlsntus55@gmail.com>
2024-06-27 00:18:12 +0900

    Title: 등장횟수 확인, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240627/등장횟수 확인/check-number-of-appearances.java
+++ 240627/등장횟수 확인/check-number-of-appearances.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] numbers = new int[5];
-        int cnt2=0;
-        for(int i = 1 ; i<5;i++){
+        int cnt2 = 0;
+
+        // 5개의 숫자를 입력 받음
+        for (int i = 0; i < 5; i++) {
             numbers[i] = sc.nextInt();
-            if(i%2==0){
+        }
+
+        // 입력받은 숫자 중 짝수의 개수를 세기
+        for (int i = 0; i < 5; i++) {
+            if (numbers[i] % 2 == 0) {
                 cnt2++;
             }
         }
+
         System.out.print(cnt2);
     }
 }
