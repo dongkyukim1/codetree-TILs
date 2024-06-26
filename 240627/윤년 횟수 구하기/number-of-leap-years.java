1b998818ff5e73ab6fa1b22b143ffe27b3f23f89
dongkyukim1 <wlsntus55@gmail.com>
2024-06-27 00:10:31 +0900

    Title: 윤년 횟수 구하기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240627/윤년 횟수 구하기/number-of-leap-years.java
+++ 240627/윤년 횟수 구하기/number-of-leap-years.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int year4 = sc.nextInt();
-        int cnt = 0;
+        int year = sc.nextInt();
+        int leapYearCount = 0;
 
-        for(int i =1 ; i<year4; i++){
-            if(i%4==0){
-                cnt++;
-            }else if(i%100==0 && i%400!=0){
-                cnt--;
+        for (int i = 1; i <= year; i++) {
+            if (i % 4 == 0) {
+                if (i % 100 == 0) {
+                    if (i % 400 == 0) {
+                        leapYearCount++;
+                    }
+                } else {
+                    leapYearCount++;
+                }
             }
         }
-         System.out.print(cnt);
+
+        System.out.print(leapYearCount);
     }
 }
